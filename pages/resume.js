a69875357bd6e178839598f2d545b9f660ea52eb
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-28 19:02:04 +0530

    resume download feature added

--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,24 @@
 import styles from "../styles/Resume.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { saveAs } from "file-saver";
 export default function Resume() {
   return (
-    <div className={styles.resume}>
-      <img src="/resume.jpg" alt="" />
+    <div className={styles.container}>
+      <Link className={styles.back_link} href="/">
+        {"<Back"}
+      </Link>
+      <div className={styles.resume}>
+        <Image width={600} height={800} src="/resume.jpg" alt="" />
+      </div>
+      <button
+        onClick={() => {
+          saveAs("/resume.pdf", "resume-sayar.pdf");
+        }}
+        className={styles.download_button}
+      >
+        Download
+      </button>
     </div>
   );
 }
