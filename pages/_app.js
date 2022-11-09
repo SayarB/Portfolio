e017c1712d58191040a17e67fa6ea88c95b38db6
SayarB <sayarbhattacharyya03@gmail.com>
2022-11-09 16:29:27 +0530

    fix: added google site verification meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+  <meta name="google-site-verification" content="gyVaz2OoScHGNTlVrtWiGtAn6CzofalUBqScikrvwqg" />
+  </Head>
+  <Component {...pageProps} /></>
 }
 
 export default MyApp
